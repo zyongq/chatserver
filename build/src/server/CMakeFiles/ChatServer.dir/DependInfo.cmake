e7be1a13ebdc773f9206ed7e4606e1460595e66e
zyongq <1115639803@qq.com>
2024-06-02 01:35:11 -0700

    buildFile

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/deeplu/Desktop/CHAT/chatserver/src/server/chatserver.cpp" "/home/deeplu/Desktop/CHAT/chatserver/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/deeplu/Desktop/CHAT/chatserver/src/server/chatservice.cpp" "/home/deeplu/Desktop/CHAT/chatserver/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/deeplu/Desktop/CHAT/chatserver/src/server/db/db.cpp" "/home/deeplu/Desktop/CHAT/chatserver/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/deeplu/Desktop/CHAT/chatserver/src/server/main.cpp" "/home/deeplu/Desktop/CHAT/chatserver/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/deeplu/Desktop/CHAT/chatserver/src/server/model/friendmodel.cpp" "/home/deeplu/Desktop/CHAT/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/home/deeplu/Desktop/CHAT/chatserver/src/server/model/groupmodel.cpp" "/home/deeplu/Desktop/CHAT/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/deeplu/Desktop/CHAT/chatserver/src/server/model/offlinemessagemodel.cpp" "/home/deeplu/Desktop/CHAT/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/deeplu/Desktop/CHAT/chatserver/src/server/model/usermodel.cpp" "/home/deeplu/Desktop/CHAT/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/deeplu/Desktop/CHAT/chatserver/src/server/redis/redis.cpp" "/home/deeplu/Desktop/CHAT/chatserver/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../thirdparty"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
